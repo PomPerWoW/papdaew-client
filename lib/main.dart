72e9a60b44678a79f8b424583a1bf384fb1c4fac
porfai <popeachyyy@gmail.com>
2025-02-19 00:15:56 +0700

    Done

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'PapDaewEIEIEI',
+      title: 'PapDaew',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
