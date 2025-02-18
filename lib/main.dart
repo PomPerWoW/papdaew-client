906d92ea9d27edc86c141e9b53ca733fce5fcfdb
shoviper <65011304@kmitl.ac.th>
2025-02-18 23:58:05 +0700

    Test1

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'PapDaew',
+      title: 'PapDaewEIEIEI',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
