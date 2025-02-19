e4d380f29a125207c7dd035d139310d6fa63a341
shoviper <65011304@kmitl.ac.th>
2025-02-20 00:56:02 +0700

    feat(signup): skeleton and some decoration of the page

---
+++ lib/presentation/views/pages/signup_page.dart
@@ -0,0 +1,175 @@
+import 'package:flutter/material.dart';
+
+class SignupPage extends StatelessWidget {
+  const SignupPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Align(
+          alignment: Alignment.bottomCenter,
+          child: Container(
+            height: MediaQuery.of(context).size.height * 0.73,
+            width: double.infinity,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadiusDirectional.only(
+                topStart: Radius.circular(40),
+                topEnd: Radius.circular(40),
+              ),
+              color: Colors.white,
+              ),
+              child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                SizedBox(height: 16),
+                Text(
+                        'Back to Login',
+                        style: TextStyle(
+                        fontSize: 14,
+                        fontWeight: FontWeight.w500,
+                        ),
+                      ),
+                SizedBox(height: 16),
+                Text(
+                  'Signup',
+                  style: TextStyle(
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                  ),
+                ),
+                SizedBox(height: 16),
+                Align(
+                  alignment: Alignment.bottomLeft,
+                  child: Text(
+                          'Email',
+                          style: TextStyle(
+                          fontSize: 14,
+                          fontWeight: FontWeight.normal,
+                          ),
+                        ),
+                ),
+                SizedBox(height: 8),
+                TextField(
+                  decoration: InputDecoration(
+                    hintText: 'Username@gmail.com',
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
+                      borderSide: BorderSide.none,
+                    ),
+                    fillColor: Color(0xFFF5F5F5),
+                    filled: true,
+                    contentPadding: EdgeInsets.symmetric(
+                      vertical: 10.0, 
+                      horizontal: 20.0
+                    ), 
+                  ),
+                ),
+                SizedBox(height: 16),
+                Align(
+                  alignment: Alignment.bottomLeft,
+                  child: Text(
+                          'Password',
+                          style: TextStyle(
+                          fontSize: 14,
+                          fontWeight: FontWeight.normal,
+                          ),
+                        ),
+                ),
+                SizedBox(height: 8),
+                TextField(
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    hintText: 'Enter your password',
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
+                      borderSide: BorderSide.none,
+                    ),
+                    fillColor: Color(0xFFF5F5F5),
+                    filled: true,
+                    contentPadding: EdgeInsets.symmetric(
+                      vertical: 10.0, 
+                      horizontal: 20.0
+                    ), 
+                  ),
+                ),
+                SizedBox(height: 16),
+                Align(
+                  alignment: Alignment.bottomLeft,
+                  child: Text(
+                          'Confirm Password',
+                          style: TextStyle(
+                          fontSize: 14,
+                          fontWeight: FontWeight.normal,
+                          ),
+                        ),
+                ),
+                SizedBox(height: 8),
+                TextField(
+                  decoration: InputDecoration(
+                    hintText: 'Confirm your password',
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
+                      borderSide: BorderSide.none,
+                    ),
+                    fillColor: Color(0xFFF5F5F5),
+                    filled: true,
+                    contentPadding: EdgeInsets.symmetric(
+                      vertical: 10.0, 
+                      horizontal: 20.0
+                    ), 
+                  ),
+                ),
+                SizedBox(height: 16),
+                Align(
+                  alignment: Alignment.bottomLeft,
+                  child: Text(
+                          'Phone',
+                          style: TextStyle(
+                          fontSize: 14,
+                          fontWeight: FontWeight.normal,
+                          ),
+                        ),
+                ),
+                SizedBox(height: 8),
+                TextField(
+                  decoration: InputDecoration(
+                    hintText: '08999999999',
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
+                      borderSide: BorderSide.none,
+                    ),
+                    fillColor: Color(0xFFF5F5F5),
+                    filled: true,
+                    contentPadding: EdgeInsets.symmetric(
+                      vertical: 10.0, 
+                      horizontal: 20.0
+                    ), 
+                  ),
+                ),
+                SizedBox(height: 40),
+                SizedBox(
+                  width: double.infinity,
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                    backgroundColor: Color(0xFF525252), // Button color
+                    ),
+                    onPressed: () {
+                      // Handle login
+                    },
+                    child: Text(
+                      'Signup',
+                      style: TextStyle(color: Colors.white),
+                      ),
+                    ),
+                ),
+                SizedBox(height: 16),
+              ],
+            ),
+          ),
+        ),
+      )
+    );
+  }
+}
