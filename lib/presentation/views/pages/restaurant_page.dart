46f6238ac8f9d775b5a5af5327fa6003d4e0c63c
shoviper <65011304@kmitl.ac.th>
2025-02-25 00:34:43 +0700

    Redesign some UI of the restaurant page

--- lib/presentation/views/pages/restaurant_page.dart
+++ lib/presentation/views/pages/restaurant_page.dart
@@ -74,9 +74,7 @@ class _RestaurantPageState extends State<RestaurantPage> {
         child: Column(
           children: [
             const SizedBox(height: 16),
-            Padding(
-              padding: const EdgeInsets.only(left: 18.0),
-              child: SizedBox(
+            SizedBox(
                 width: MediaQuery.of(context).size.width * 0.9,
                 height: 50,
                 child: SearchBar(
@@ -90,7 +88,6 @@ class _RestaurantPageState extends State<RestaurantPage> {
                   ),
                 ),
               ),  
-            ),
             const SizedBox(height: 16),
             SizedBox(
               height: MediaQuery.of(context).size.height * 0.13,
@@ -203,123 +200,80 @@ class _RestaurantPageState extends State<RestaurantPage> {
               ),
             ),
             SizedBox(height: 16),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Align(
-                  alignment: Alignment.topLeft,
-                  child: Padding(
-                    padding: const EdgeInsets.only(left: 18.0),
-                    child: Text(
-                      'All restaurant',
-                      style: TextStyle(
-                        color: Colors.black,
-                        fontSize: 20,
-                        fontWeight: FontWeight.bold,
-                        fontFamily: "Inter",
-                      ),
-                    ),
+            Align(
+              alignment: Alignment.topLeft,
+              child: Padding(
+                padding: const EdgeInsets.only(left: 18.0),
+                child: Text(
+                  'Restaurant near you',
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                    fontFamily: "Inter",
                   ),
                 ),
-                Align(
-                  alignment: Alignment.topRight,
-                  child: Padding(
-                    padding: const EdgeInsets.only(right: 18.0),
-                    child: Text(
-                      'see all',
-                      style: TextStyle(
-                        color: Color(0xFFA4C3B2),
-                        fontSize: 12,
-                        fontWeight: FontWeight.bold,
-                        fontFamily: "Inter",
-                      ),
-                    ),
-                  ),
-                ),
-              ],
+              ),
             ),
             SizedBox(height: 8),
-            Container(
-              width: 380,
-              height: 110,
-              decoration: BoxDecoration(
+            GridView.builder(
+              shrinkWrap: true,
+              physics: NeverScrollableScrollPhysics(),
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 1,
+              crossAxisSpacing: 16,
+              mainAxisSpacing: 16,
+              childAspectRatio: 3 / 1,
+              ),
+              padding: const EdgeInsets.symmetric(horizontal: 18),
+              itemCount: 3,
+              itemBuilder: (context, index) {
+              return Container(
+                decoration: BoxDecoration(
                 borderRadius: BorderRadius.circular(16),
                 color: Colors.grey,
-              ),
+                ),
+              );
+              },
             ),
             SizedBox(height: 16),
-            Container(
-              width: 380,
-              height: 110,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(16),
-                color: Colors.grey,
+            Align(
+              alignment: Alignment.topLeft,
+              child: Padding(
+                padding: const EdgeInsets.only(left: 18.0),
+                child: Text(
+                  'Out of Area',
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                    fontFamily: "Inter",
+                  ),
+                ),
               ),
             ),
-            SizedBox(height: 16),
-            Container(
-              width: 380,
-              height: 110,
-              decoration: BoxDecoration(
+            SizedBox(height: 8),
+            GridView.builder(
+              shrinkWrap: true,
+              physics: NeverScrollableScrollPhysics(),
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 1,
+              crossAxisSpacing: 16,
+              mainAxisSpacing: 16,
+              childAspectRatio: 3 / 1,
+              ),
+              padding: const EdgeInsets.symmetric(horizontal: 18),
+              itemCount: 8,
+              itemBuilder: (context, index) {
+              return Container(
+                decoration: BoxDecoration(
                 borderRadius: BorderRadius.circular(16),
                 color: Colors.grey,
-              ),
+                ),
+              );
+              },
             ),
-            SizedBox(height: 16),
-            Container(
-              width: 380,
-              height: 110,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(16),
-                color: Colors.grey,
-              ),
-            ),
-            SizedBox(height: 16),
-            Container(
-              width: 380,
-              height: 110,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(16),
-                color: Colors.grey,
-              ),
-            ),
-            SizedBox(height: 16),
-            Container(
-              width: 380,
-              height: 110,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(16),
-                color: Colors.grey,
-              ),
-            ),
-            SizedBox(height: 16),
-            Container(
-              width: 380,
-              height: 110,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(16),
-                color: Colors.grey,
-              ),
-            ),
-            SizedBox(height: 16),
-            Container(
-              width: 380,
-              height: 110,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(16),
-                color: Colors.grey,
-              ),
-            ),
-            SizedBox(height: 16),
-            Container(
-              width: 380,
-              height: 110,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(16),
-                color: Colors.grey,
-              ),
-            ),
-            
+            SizedBox(height: MediaQuery.of(context).size.height * 0.08),
           ],
         ),
       )
